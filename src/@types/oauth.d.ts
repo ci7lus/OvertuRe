c6137e9fb2dbe09bc659853e1fc3fa4296ae33d4
ci7lus <7887955+ci7lus@users.noreply.github.com>
2021-06-09 14:22:35 +0900

    :arrow_up: 依存更新

--- src/@types/oauth.d.ts
+++ src/@types/oauth.d.ts
@@ -1,23 +1,13 @@
 declare namespace GoogleAppsScript {
   interface OAuth {
-    createService: (
-      name: string
-    ) => {
-      setAccessTokenUrl: (
-        s: string
-      ) => {
-        setRequestTokenUrl: (
-          s: string
-        ) => {
-          setAuthorizationUrl: (
-            s: string
-          ) => {
-            setConsumerKey: (
-              s: string
-            ) => {
-              setConsumerSecret: (
-                s: string
-              ) => { setAccessToken: (at: string, ats: string) => OAuth }
+    createService: (name: string) => {
+      setAccessTokenUrl: (s: string) => {
+        setRequestTokenUrl: (s: string) => {
+          setAuthorizationUrl: (s: string) => {
+            setConsumerKey: (s: string) => {
+              setConsumerSecret: (s: string) => {
+                setAccessToken: (at: string, ats: string) => OAuth
+              }
             }
           }
         }
